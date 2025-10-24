95890c6dd3a7785c3674426cf55b1665e2049afe
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2025-10-24 23:54:12 +1300

    fix: preload works

--- src/routes/work/index.tsx
+++ src/routes/work/index.tsx
@@ -4,25 +4,29 @@ import PageLayout from "~/components/PageLayout";
 import { Suspense } from "solid-js";
 import Spinner from "~/components/Spinner";
 
-export default function WorkHome() {
-	const works = createAsync(() => getWorks());
+export const route = {
+  preload: () => getWorks(),
+};
 
-	return (
-		<PageLayout title="Work" description="Some work that I have made">
-			<h1>Work</h1>
-			<p>Some work that I have made.</p>
-			<Suspense fallback={<Spinner color="grey" />}>
-				{works() ? (
-					works()!.map((work) => (
-						<a href={`/work/${work.slug}`}>
-							<h2>{work.title}</h2>
-							<p>{work.summary}</p>
-						</a>
-					))
-				) : (
-					<p>Loading...</p>
-				)}
-			</Suspense>
-		</PageLayout>
-	);
+export default function WorkHome() {
+  const works = createAsync(() => getWorks());
+  
+  return (
+    <PageLayout title="Work" description="Some work that I have made">
+      <h1>Work</h1>
+      <p>Some work that I have made.</p>
+      <Suspense fallback={<Spinner color="grey" />}>
+        {works() ? (
+          works()!.map((work) => (
+            <a href={`/work/${work.slug}`}>
+              <h2>{work.title}</h2>
+              <p>{work.summary}</p>
+            </a>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Suspense>
+    </PageLayout>
+  );
 }
