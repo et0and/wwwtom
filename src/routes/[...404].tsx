97c13b292f104eee546bf5526056632d8efeca2f
Tom Hackshaw <gh@tomhackshaw.com>
2025-11-27 18:25:07 +1300

    fix: title in 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,7 +1,13 @@
+import PageLayout from "~/components/PageLayout";
+
 export default function NotFound() {
 	return (
-		<main class="text-center mx-auto p-4">
-			<h1>Not Found</h1>
-		</main>
+		<PageLayout
+			title="404"
+			description="The page you are looking for does not exist."
+		>
+			{" "}
+			<h1 class="text-center mx-auto p-4">Not found</h1>
+		</PageLayout>
 	);
 }
