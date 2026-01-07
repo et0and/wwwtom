9bdc723f66fff03fffe4a3cb0f34efe47d73f05f
Tom Hackshaw <gh@tomhackshaw.com>
2026-01-07 23:02:50 +1300

    feat: favicon

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -246,6 +246,14 @@ app.get(
   }),
 );
 
-app.get("/", Scalar({ url: "/openapi", theme: "elysiajs", pageTitle: "Tom API", favicon: "https://tom-so.vercel.app/favicon.ico" }));
+app.get(
+  "/",
+  Scalar({
+    url: "/openapi",
+    theme: "elysiajs",
+    pageTitle: "Tom API",
+    favicon: "https://tom-so.vercel.app/favicon.ico",
+  }),
+);
 
 export default app;
