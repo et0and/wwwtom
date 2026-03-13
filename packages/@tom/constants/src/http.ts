70dcb4c5bc3c7d441992c03c777075d2b55eb23e
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2026-03-14 12:09:34 +1300

    fix(packages): use type safe const obj, not loose enums

--- packages/@tom/constants/src/http.ts
+++ packages/@tom/constants/src/http.ts
@@ -1,17 +1,20 @@
-export enum HttpStatus {
-  Ok = 200,
-  Created = 201,
-  Accepted = 202,
-  NoContent = 204,
-  Found = 302,
-  BadRequest = 400,
-  Unauthorized = 401,
-  Forbidden = 403,
-  NotFound = 404,
-  ImATeapot = 418,
-  UnprocessableEntity = 422,
-  InternalServerError = 500,
-  BadGateway = 502,
-  ServiceUnavailable = 503,
-  GatewayTimeout = 504,
-}
+export const HttpStatus = {
+  Ok: 200,
+  Created: 201,
+  Accepted: 202,
+  NoContent: 204,
+  Found: 302,
+  BadRequest: 400,
+  Unauthorized: 401,
+  Forbidden: 403,
+  NotFound: 404,
+  ImATeapot: 418,
+  UnprocessableEntity: 422,
+  InternalServerError: 500,
+  BadGateway: 502,
+  ServiceUnavailable: 503,
+  GatewayTimeout: 504,
+  FunnyMemeStatus: 67,
+} as const;
+
+export type HttpStatus = typeof HttpStatus[keyof typeof HttpStatus];
