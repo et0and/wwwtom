5c2251d8f6d357ea7263451fa5706df0ae8e8d7d
et0and <gh@tomhackshaw.com>
2026-09-24 13:42:18 +1200

    fix(web): remove nav background colors

--- packages/ui/src/Nav.tsx
+++ packages/ui/src/Nav.tsx
@@ -23,7 +23,7 @@ export function Nav() {
   );
 
   return (
-    <nav class="relative md:sticky md:top-0 tracking-tighter px-6 py-4 flex-shrink-0 view-transition-header bg-white dark:bg-[#0a0a0a] z-50">
+    <nav class="relative md:sticky md:top-0 tracking-tighter px-6 py-4 flex-shrink-0 view-transition-header z-50">
       <div class="flex items-center justify-between h-16">
         <a class="font-medium" href="/">
           <h1 class="!text-lg">Tom Hackshaw</h1>
